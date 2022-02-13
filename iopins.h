89c3d5c1389f8a84ed48fc2ef35fa468417bcee7
Krzysztof Iwanowski <kiwanowski0@gmail.com>
2022-02-13 22:30:16 +0100

    first commit

---
+++ iopins.h
@@ -0,0 +1,18 @@
+#ifndef IOPINS_H
+#define IOPINS_H
+
+#include "platform_config.h"
+
+#define TFT_SCLK        13
+#define TFT_MOSI        11
+#define TFT_MISO        12
+#define TFT_TOUCH_CS    255
+#define TFT_TOUCH_INT   255
+#define TFT_DC          2
+#define TFT_CS          10 // 255 for LORES ST7789 (NO CS)
+#define TFT_RST         3 // 255 for ILI/ST if connected to 3.3V
+
+#define PIN_JOY2_A1X    0
+#define PIN_JOY2_A2Y    0
+
+#endif
---
+++ platform_config.h
@@ -0,0 +1,12 @@
+#ifndef _PLATFORM_CONFIG_H_
+#define _PLATFORM_CONFIG_H_
+
+#define HAS_SND        1
+#define ILI9341        1
+#define SWAP_JOYSTICK  1
+#define USE_SDFAT      1
+#define SD_FAT_TYPE    1
+//#define USE_SDFS       1
+//#define SDFSDEV        "1:"
+
+#endif
